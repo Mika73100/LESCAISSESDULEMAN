f936354f3d0c5fa5a4a4cf644e7741c4222d278b
Michael BARRECA <Michael-73@live.Fr>
2026-01-20 21:42:03 +0100

    modification html

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -74,13 +74,13 @@ const LandingPage = () => {
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z" fill="#10B981"/>
                 </svg>
-                <span>+100 projets web & graphisme livrés</span>
+                <span>+100 caisses enregistreuses livrées</span>
               </div>
               <div className="hero-metric">
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z" fill="#10B981"/>
                 </svg>
-                <span>+200 clients accompagnés</span>
+                <span>+200 projets web accompagnés</span>
               </div>
             </div>
             <div className="hero-buttons">
